11ba474f81cebd1d9c27197faaa279c93fcbc948
Cooper Cain <coopercc@uw.edu>
2015-11-16 12:24:00 -0800

    Add js file

---
+++ js/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+angular.module('LoginApp', [])
+.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
+
+	if (/* Form name is valid*/) {
+		//then do whatever
+		
+	}
+
+}])
