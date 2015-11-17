823e46375442b65b2d9c9c9e4a1d52a779ca61da
Peter Freschi <pfreschi@uw.edu>
2015-11-16 23:02:35 -0800

    included bootstrap and added two password validations Fixes #4

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
-"use strict";
+'use strict';
 
-angular.module('LoginApp', [])
-.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
+angular.module('LoginApp', ['ngSanitize'])
+	.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
+		$scope.today = Date.now();
 
-	if (/* Form name is valid*/) {
-		//then do whatever
+
+		$scope.validatePW = function(){
+			
+			var pass1 = $scope.pass1;
+			var pass2 = $scope.pass2;
+
+			if (pass1 == pass2) {
+				console.log("they match");
+				$scope.signup.pass2.$setValidity("required", true);
+			} else {
+				console.log("they don't match");
+				$scope.signup.pass2.$setValidity("required", false);
+			}
+			
+		};
+
 		
-	}
 
-}])
+	}])
