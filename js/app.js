8c92902c374b312c870ee7e8831639384099d5dd
Peter Freschi <pfreschi@uw.edu>
2015-11-17 19:08:26 -0800

    added password validation and test Fixes #3 , and fixed input styling

--- js/app.js
+++ js/app.js
@@ -2,22 +2,36 @@
 
 angular.module('LoginApp', ['ngSanitize'])
 	.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
-		$scope.today = Date.now();
-
-		$scope.validatePW = function(){
-			
+		
+		$scope.validatePW = function(){	
 			var pass1 = $scope.pass1;
 			var pass2 = $scope.pass2;
-
 			if (pass1 == pass2) {
-				console.log("they match");
 				$scope.signup.pass2.$setValidity("required", true);
 			} else {
-				console.log("they don't match");
 				$scope.signup.pass2.$setValidity("required", false);
 			}
 			
 		};
 
+		$scope.validateBD = function(){
+			var today = new Date();
+			var birthdate = Date.parse($scope.birthdateBox);
+			if (/^\d+$/.test(birthdate)) {
+				birthdate = new Date(birthdate);
+				var age = today.getFullYear() - birthdate.getFullYear();
+				var m = today.getMonth() - birthdate.getMonth();
+				if (m < 0 || (m === 0 && today.getDate() < birthdate.getDate())){
+					age--;
+				}
+				if (age >= 13 && age) {
+					$scope.signup.birthdateBox.$setValidity("required", true);
+				} else {
+					$scope.signup.birthdateBox.$setValidity("required", false);
+				}
 
+			} else {
+				$scope.signup.birthdateBox.$setValidity("required", false);
+			}
+		};
 	}])
